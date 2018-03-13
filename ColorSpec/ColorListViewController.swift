41d8ac7276be93aad34fa0dbc5b193679d99b0af
Kevin Randrup <kevinrandrup@gmail.com>
2018-03-12 20:57:15 -0400

    Part 1 - constant reference back to ColorListViewController

---
+++ ColorSpec/ColorListViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ColorListViewController.swift
+//  ColorSpec
+//
+//  Created by Kevin Randrup on 3/12/18.
+//  Copyright © 2018 Kevin Randrup. All rights reserved.
+//
+
+import UIKit
+
+class ColorListViewController : UIViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    var colors: [UIColor] = [UIColor.blue]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        collectionView.dataSource = self
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Are we going to the color picker?
+        if let destination = segue.destination as? ColorPickerViewController {
+            
+            // Do we have a color to give it?
+            if let cell = sender as? UICollectionViewCell,
+                let indexPath = collectionView.indexPath(for: cell) {
+                let color = colors[indexPath.row]
+                destination.initialize(startingColor: color, callback: self)
+            }
+        }
+    }
+    
+    func heyManIEditedThisColor(color: UIColor) {
+        colors[0] = color
+        collectionView.reloadData()
+    }
+}
+
+extension ColorListViewController : UICollectionViewDataSource {
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return colors.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let view = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+        view.backgroundColor = colors[indexPath.row]
+        return view
+    }
+}
+
---
+++ ColorSpec/ColorPickerViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ColorPickerViewController.swift
+//  ColorSpec
+//
+//  Created by Kevin Randrup on 3/12/18.
+//  Copyright © 2018 Kevin Randrup. All rights reserved.
+//
+
+import UIKit
+
+class ColorPickerViewController: UIViewController {
+    
+    func initialize(startingColor: UIColor, callback: ColorListViewController) {
+        self.startColor = startingColor
+        self.callback = callback
+    }
+
+    //MARK: - Properties
+
+    var callback: ColorListViewController?
+
+    private var startColor: UIColor = .white
+    
+    @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    var currentColor: UIColor {
+        let red = CGFloat(redSlider.value) / 256
+        let green = CGFloat(greenSlider.value) / 256
+        let blue = CGFloat(blueSlider.value) / 256
+        return UIColor(red: red, green: green, blue: blue, alpha: 1.0)
+    }
+    
+    override func viewDidLoad() {
+        self.colorView.backgroundColor = self.startColor
+        
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        startColor.getRed(&red, green: &green, blue: &blue, alpha: nil)
+        redSlider.value = 256 * Float(red)
+        greenSlider.value = 256 * Float(green)
+        blueSlider.value = 256 * Float(blue)
+    }
+    
+    
+    //MARK: - User Interaction
+    
+    @IBAction func updateColor() {
+        self.colorView.backgroundColor = self.currentColor
+    }
+    
+    @IBAction func donePressed(_ sender: Any) {
+        self.callback?.heyManIEditedThisColor(color: self.currentColor)
+        self.navigationController?.popViewController(animated: true)
+    }
+}
+
+
